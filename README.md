2d08f6d79dfdb9fca4f0ee4cb215bfe92e36570e
Jonan Scheffler <jonanscheffler@gmail.com>
2020-03-30 22:39:34 -0700

    Fix .gitignore, prepare for launch.

---
+++ README.md
@@ -0,0 +1,103 @@
+# Wikistream
+
+A Python package that uses [`aiosseclient`](https://github.com/ebraminio/aiosseclient) to stream edits to Wikimedia properties around the world and store them in [TimescaleDB](https://timescale.com).
+
+This package assumes that you're accessing a PostgreSQL database on your local
+machine that already has the TimescaleDB extension installed and enabled. [TimescaleDB Docker Image](https://hub.docker.com/r/timescale/timescaledb/)
+
+It also assumes that said database has a user `postgres` with a password of `password`. If you're using the aforementioned Docker image to setup TimescaleDB you can set the password with an environment variable:
+
+```
+POSTGRES_PASSWORD=password
+```
+You don't need to do anything to set the user to `postgres`, it's already the default.
+
+## That's a lot of assumptions, seems pretty inflexible.
+
+You're right, it is. This is intended for use with a TimescaleDB demo application
+and is probably not fit for much else. You're of course welcome to the code anyway,
+perhaps you will find something useful.
+
+## Usage
+Create a Wikistream:
+
+```python
+ws = Wikistream()
+
+# default config:
+# host: localhost
+# port: 5432
+# user: postgres
+# password: password
+# dbname: wikistream
+# table: events
+```
+
+Start it:
+
+```python
+ws.start()
+```
+
+Alternatively pass in your own artisanal handcrafted configuration fancy-pants:
+
+```python
+config = {
+    "host": "weird_host",
+    "port": "port_with_likely_conflicts",
+    "user": "confusing_pg_username",
+    "password": "easily_guessed_password",
+    "dbname": "something_less_cool_than_wikistream_prolly",
+    "table": "arbitrary and ambiguous table name"
+}
+
+ws = Wikistream(config)
+ws.start()
+```
+
+## Batching
+Rather than insert each record individually Wikistream will save up 50 at a time
+and use `execute_batch` to insert them all at once. You can change the batch size
+in the config:
+
+```python
+config = {
+    "batch": 50
+}
+```
+
+## Do overs
+Occasionally Wikistream has trouble streaming new events from the Wikimedia service
+that's providing our stream ([https://stream.wikimedia.org/v2/stream/recentchange](https://stream.wikimedia.org/v2/stream/recentchange))
+because the internet is a hellscape.
+
+In the unlikely event of a water landing and/or connection error Wikistream will take a brief nap,
+or rather a series of naps with decreasing brevity, before surrendering to the will of the tubes.
+
+You can configure sleepy time by providing an array of sleep durations in the config:
+
+```python
+config = {
+    "retries": [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+}
+
+# these are the default values, durations in seconds
+```
+
+## Logging
+Wikistream provides 5 log levels of increasing severity: `debug`, `info`, `warn`, `error` and `fatal`.
+
+You can set `log_level` in your config to see all messages at that level or greater:
+
+```python
+config = {
+    "log_level": "info"
+    
+    # the default of 'info' will show all of the log messages except 'debug'
+    # a log_level of 'error' would only show logs with level 'error' and 'fatal'
+}
+```
+
+## License
+MIT
+Copyright (c) 2020 Jonan Scheffler ([LICENSE](https://github.com/timescale/wikistream/LICENSE.md))
--- wikistream/__main__.py
+++ wikistream/__main__.py
@@ -1,104 +1,149 @@
 import asyncio
 import aiohttp
-import json
-import psycopg2
-import psycopg2.extras
 from aiosseclient import aiosseclient
+import sqlalchemy
+
+import re
+import json
+import time
+import datetime
+from datetime import datetime, timezone
 
 class Wikistream:
-    def __init__(self):
-        self.BATCH_SIZE = 50
-        self.CONFIG = {
+    def __init__(self, config={}):
+        self.config = {
             "host": "localhost",
+            "port": 5432,
             "user": "postgres",
             "password": "password",
-            "dbname": "wikistream"
+            "dbname": "wikistream",
+            "table": "events",
+            "batch": 50,
+            "retries": [0, 1, 1, 2, 3, 5, 8, 13, 21, 34],
+            "log_level": "info"
         }
-        self.record_list = []
-        self.connection = None
+        self.config.update(config)
+        self.log_levels = ["debug", "info", "warn", "error", "fatal"]
+        self.wikimedia_url = "https://stream.wikimedia.org/v2/stream/recentchange"
+        self.database_url = f"postgresql://{self.config['user']}:{self.config['password']}@{self.config['host']}:{self.config['port']}/{self.config['dbname']}"
+        self.safe_database_url = re.sub(self.config["password"], "********", self.database_url)
+        self.engine = sqlalchemy.create_engine(self.database_url, executemany_mode="batch")
+
+        self.queued_events = []
+        self.table = None
 
     def start(self):
-        self.setup_database()
-
-        self.connection = psycopg2.connect(
-            host=self.CONFIG["host"],
-            user=self.CONFIG["user"],
-            password=self.CONFIG["password"],
-            dbname=self.CONFIG["dbname"]
-        )
-        cursor = self.connection.cursor()
-
-        try:
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(self.stream(cursor))
-        finally:
-            cursor.close()
-            self.connection.close()
-
-    async def stream(self, cursor):
-        async for event in aiosseclient('https://stream.wikimedia.org/v2/stream/recentchange'):
-            print(event)
-            self.save(event, cursor)
-
-    def setup_database(self):
-        self.create_database()
         self.create_table()
+        self.log("info", f"Connected to {self.database_url}")
+        self.log("info", f"Streaming from {self.wikimedia_url}...")
 
-    def create_database(self):
-        connection = psycopg2.connect(
-            host=self.CONFIG["host"],
-            user=self.CONFIG["user"],
-            password=self.CONFIG["password"]
-        )
-        connection.autocommit = True
-        cursor = connection.cursor()
+        while True:
+            for attempt, duration in enumerate(self.config["retries"]):
+                attempt_time = time.time()
+                try:
+                    self.log("info", f"Starting the run loop... {attempt}/{len(self.config['retries'])}")
+                    self.run_loop()
+                except asyncio.TimeoutError:
+                    self.log("warn", f"Timed out attempting to stream from '{self.wikimedia_url}'. Retrying in {duration} seconds... ({attempt}/{len(self.config['retries'])})")
+                    time.sleep(duration)
+                    error_time = time.time()
+                    if ((error_time - attempt_time) > max(self.config["retries"]) + 1):
+                        self.log("info", f"Resetting retry counter after {error_time - attempt_time} seconds.")
+                        break
+                    else:
+                        self.log("info", f"Retrying after {error_time - attempt_time} seconds...")
+            else:
+                self.log("fatal", f"Timed out {len(self.config['retries'])} times trying to stream from '{self.wikimedia_url}'.")
+                break
 
-        try:
-            dbname = self.CONFIG["dbname"]
-            cursor.execute(f"CREATE DATABASE {dbname};")
-        except psycopg2.errors.DuplicateDatabase:
-            print(f"Database {dbname} already exists.")
-        finally:
-            cursor.close()
-            connection.close()
+    def run_loop(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.stream())
+
+    async def stream(self):
+        async for event in aiosseclient(self.wikimedia_url):
+            self.save(event)
 
     def create_table(self):
-        connection = psycopg2.connect(
-            host=self.CONFIG["host"],
-            user=self.CONFIG["user"],
-            password=self.CONFIG["password"],
-            dbname=self.CONFIG["dbname"]
+        metadata = sqlalchemy.MetaData()
+        self.table = sqlalchemy.Table(self.config["table"], metadata,
+            sqlalchemy.Column("time", sqlalchemy.DateTime, nullable=False),
+            sqlalchemy.Column("wiki_user", sqlalchemy.Text, nullable=False),
+            sqlalchemy.Column("comment_length", sqlalchemy.Integer, nullable=False),
+            sqlalchemy.Column("domain", sqlalchemy.Text, nullable=False),
+            sqlalchemy.Column("raw", sqlalchemy.dialects.postgresql.JSONB, nullable=False),
         )
-        cursor = connection.cursor()
+        metadata.create_all(self.engine)
 
-        try:
-            cursor.execute(f"CREATE TABLE events (time TIMESTAMPTZ NOT NULL, raw JSONB NOT NULL, domain TEXT NOT NULL, wiki_user TEXT NOT NULL, comment_length NUMERIC NOT NULL);")
-            cursor.execute("SELECT create_hypertable('events', 'time');")
-            cursor.execute("ALTER TABLE events SET (timescaledb.compress, timescaledb.compress_segmentby = 'wiki_user');")
-            cursor.execute("SELECT add_compress_chunks_policy('events', INTERVAL '5 minutes');")
-            connection.commit()
-        except psycopg2.errors.DuplicateTable:
-            print("Table events already exists.")
-        finally:
-            cursor.close()
-            connection.close()
+        with self.engine.connect() as connection:
+            table = self.config["table"]
 
-    def save(self, event, cursor):
+            try:
+                connection.execute(f"SELECT create_hypertable('{table}', 'time');")
+            except sqlalchemy.exc.DatabaseError as error:
+                if "already a hypertable" not in str(error):
+                    self.log("error", f"Error attempting to convert '{table}' into a hypertable.", { "error": str(error), "db": self.safe_database_url })
+                    raise error
+                else:
+                    self.log("debug", f"Table '{table}' is already a hypertable.", { "db": self.safe_database_url })
+            try:
+                connection.execute(f"ALTER TABLE {table} SET (timescaledb.compress, timescaledb.compress_segmentby = 'wiki_user');")
+            except sqlalchemy.exc.NotSupportedError as error:
+                if "compressed chunks already exist" not in str(error):
+                    self.log("error", f"Error attempting to alter hypertable '{table}' for compression.", { "error": str(error), "db": self.safe_database_url })
+                    raise error
+                else:
+                    self.log("debug", f"Table '{table}' already contains compressed chunks so it could not be altered for compression.")
+            try:
+                connection.execute(f"SELECT add_compress_chunks_policy('{table}', INTERVAL '10 minutes');")
+            except sqlalchemy.exc.ProgrammingError as error:
+                if "compress chunks policy already exists" not in str(error):
+                    self.log("error", f"Error attempting to create a compress chunks policy for '{table}' for compression.", { "error": str(error), "db": self.safe_database_url })
+                    raise error
+                else:
+                    self.log("debug", f"Table {table} already has a compress chunks policy.", { "db": self.safe_database_url })
+
+    def save(self, event):
         parsed = json.loads(str(event))
 
-        time = parsed["meta"]["dt"]
-        wiki_user = parsed["user"]
-        comment_length = len(parsed["comment"])
-        domain = parsed["meta"]["domain"]
+        new_event = {
+            "time": parsed["meta"]["dt"],
+            "wiki_user": parsed["user"],
+            "comment_length": len(parsed["comment"]),
+            "domain": parsed["meta"]["domain"],
+            "raw": str(event)
+        }
 
-        record = [time, wiki_user, domain, comment_length, str(event)]
-        self.record_list.append(record)
+        self.queued_events.append(new_event)
 
-        if len(self.record_list) >= self.BATCH_SIZE:
-            print("Saving records...")
-            psycopg2.extras.execute_batch(cursor, "INSERT INTO events (time, wiki_user, domain, comment_length, raw) VALUES (%s, %s, %s, %s, %s)", self.record_list)
-            self.connection.commit()
-            self.record_list = []
+        if (len(self.queued_events) > self.config["batch"]):
+            self.log("debug", f"Inserting {len(self.queued_events)} events...", { "insert_count": len(self.queued_events), "db": self.safe_database_url })
+            with self.engine.connect() as connection:
+                connection.execute(self.table.insert(), self.queued_events)
+                self.queued_events = []
+        else:
+            self.log("debug", f"Queueing event...", { "queued_event_count": len(self.queued_events), "event": parsed })
 
-ws = Wikistream()
+    def log(self, level, message, data={}):
+        if self.log_levels.index(level) >= self.log_levels.index(self.config["log_level"]):
+            log_info = {
+                "level": level,
+                "time": str(datetime.now(timezone.utc)),
+                "tag": "wikistream",
+                "message": message
+            }
+            log_info.update(data)
+            print(json.loads(json.dumps(log_info)))
+
+dbconfig = {
+    "host": "localhost",
+    "port": 5432,
+    "user": "postgres",
+    "password": "password",
+    "dbname": "wikistream",
+    "table": "events",
+    "batch": 50
+}
+
+ws = Wikistream(dbconfig)
 ws.start()
